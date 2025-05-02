c05ec9a5ca3e9eef87ed938883f504915fa2c96a
dnzLucas <lucasdefenix@gmail.com>
2025-05-02 19:36:28 -0300

    FEAT: Create footer #13

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HeaderComp from './components/default/HeaderComp.vue'
+import FooterComp from './components/default/FooterComp.vue'
 </script>
 
 <template>
     <HeaderComp />
-
-  <RouterView />
+    <RouterView />
+    <FooterComp />
 </template>
 
 <style>
