4f29f27a9ede05ba1c9fd1bd8b2b5255c6b6e437
dnzlucas <lucadefenix@gmail.com>
2025-05-08 17:20:41 -0300

    feat: homepage creating

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,40 @@
 <script setup>
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+import Button from '../components/home/HomeButtons.vue'
+const route = useRoute()
+
+
+const links = [
+  { to: '/overworld', label: 'Overworld' },
+  { to: '/nether', label: 'Nether' },
+  { to: '/end', label: 'The end' },]
 
 </script>
 
 <template>
   <main>
+    <div class="container">
+      <div class="welcome">
+      <h1>A sua loja preferida de personagens do Minecraft! </h1>
+    </div>
+      <div class="buttons" v-for="link in links" :key="link.to">
+        <RouterLink
+          v-if="link.to !== route.path"
+          :to="link.to"
+          class="link"
+          @click="toggleMenu"
+        >
+          <Button :text="link.label" />
+        </RouterLink>
+      </div>
+
+    </div>
   </main>
 </template>
+
+<style>
+@import "../assets/scss/home/_home.scss";
+</style>
