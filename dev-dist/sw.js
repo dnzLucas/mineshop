a48a306f984aa3c2341f876c8f1415df3f0bcf27
dnzlucas <lucasdefenix@gmail.com>
2025-05-11 22:16:23 -0300

    commit changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u8peuh8t2jg"
+    "revision": "0.cnphcd7qv9"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
